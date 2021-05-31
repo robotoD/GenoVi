a48fdbd543b44484e2384c9ba45d0b0a142ae89b
Vicente Saona <vicente.saona@sansano.usm.cl>
2021-05-31 18:03:31 -0400

    Added .gbk to .faa and .gbk to .fna python3 scripts

---
+++ gbk2fasta/genbank2fna.py
@@ -0,0 +1,37 @@
+#See my webpage:
+#http://www.warwick.ac.uk/go/peter_cock/python/genbank2fasta/
+from Bio import SeqIO
+import argparse
+
+parser = argparse.ArgumentParser(description="Starting from a GenBank flat file, this simplifies it as a .fna")
+parser.add_argument("inputFile", help="flat file to be translated")
+parser.add_argument("--outputFile", help="output file name" )
+args = parser.parse_args()
+if ".gb" in args.inputFile:
+    if args.inputFile[-4] == ".":
+        args.inputFile = args.inputFile[:-4]
+    elif args.inputFile[-3] == ".":
+        args.inputFile = args.inputFile[:-3]
+if args.outputFile is None:
+    args.outputFile = args.inputFile + "_converted.fna"
+
+try:
+    input_handle  = open(args.inputFile, "r")
+except:
+    input_handle  = open(args.inputFile + ".gbk", "r")
+output_handle = open(args.outputFile, "w")
+
+#Short version:
+#SeqIO.write(SeqIO.parse(input_handle, "genbank"), output_handle, "fasta")
+
+#Long version, allows full control of fasta output
+for seq_record in SeqIO.parse(input_handle, "genbank") :
+    print("Dealing with GenBank record %s" % seq_record.id)
+    output_handle.write(">%s %s\n%s\n" % (
+           seq_record.id,
+           seq_record.description,
+           seq_record.seq))
+
+output_handle.close()
+input_handle.close()
+print("Done")

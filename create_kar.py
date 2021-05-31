7a55cbb6c9d9fe6d1164b551b96729359d550f8d
Andrea <andrea.rodriguez.12@sansano.usm.cl>
2021-06-01 00:09:37 +0100

    Generate CDS files unfinished
    Generating positive and negative CDS files without first column and color column.

--- create_kar.py
+++ create_kar.py
@@ -6,6 +6,7 @@
 from Bio import SeqIO
 import sys
 import numpy as np
+import csv
 
 ## Function to obtain contig sizes from gbk file, then computes contig locations
 ## And finally creates a kar file with original contig order.
@@ -17,7 +18,7 @@ def ends_sorted(ends):
 	indexes = list(dic_sorted.keys())
 	return ends_sort, indexes
 
-def main(gbk_filename, output_file):
+def create_kar(gbk_filename, output_file):
 	
 	gbk_file = open(gbk_filename,"r")
 
@@ -72,8 +73,72 @@ def main(gbk_filename, output_file):
 		output.writelines(lines)
 		print(output_file+" created succesfully.")
 
+
+	return ends
+
+def create_CDS(gbk_filename, cds_p_output, cds_n_output, sizes):
+	
+	def new_loc(array, sizes_x):
+		new_arr = []
+		for i, loc_pos in enumerate(array):
+			loc_pos = loc_pos.split(":")
+			init = int(loc_pos[0][1:])
+			end = int(loc_pos[1][:-1])
+			new_arr.append([init+sizes_x[i],end+sizes_x[i]])
+		return new_arr
+		
+	def write_lines(locations, sizes_x, output_):
+		lines = []
+		for i in range(len(locations)):
+			print(locations[i])
+			line = ["chr-Node_x_length_"+str(sizes_x[i])+"_cov_x"] + list(map(str, locations[i]))
+			lines.append(line)
+		with open(output_, 'w', newline='') as csvfile:
+			writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+			writer.writerows(lines)	
+
+	
+	gbk_file = open(gbk_filename,"r")
+	positives = []
+	negatives = []
+	sizes_p = []
+	sizes_n = []
+	
+	for j, record in enumerate(SeqIO.parse(gbk_file, "genbank")):
+		aux_p = []
+		aux_n = []
+		for feature in record.features:
+			if feature.type == "CDS":
+				location = str(feature.location)[:-3]
+				direction = str(feature.location)[-2:-1]
+				if direction == '+':
+					positives.append(location)
+					if j == 0:
+						aux_p.append(0)
+					else:
+						aux_p.append(sizes[j-1])
+				else:
+					negatives.append(location)
+					if j == 0:
+						aux_n.append(0)
+					else:
+						aux_n.append(sizes[j-1])
+		sizes_p = sizes_p + aux_p
+		sizes_n = sizes_n + aux_n
+	
+	new_pos = new_loc(positives, sizes_p)
+	new_negs = 	new_loc(negatives, sizes_n)
+	
+	write_lines(new_pos, sizes_p, cds_p_output)
+	write_lines(new_negs, sizes_n, cds_n_output)
+	
+	return
+				
+
 def help():
-	print("Run: python create_kar.py -i <input genbank file path> -o <output kar file path>")			
+	print("Run")
+	print("For only creating KAR file: python create_kar.py -i <input genbank file path> -o <output kar file path>")	
+	print("For creating KAR file and CDS positive and CDS negative files: python create_kar.py -i <input genbank file path> -o <output kar file path> -cp <output_CDS_positives> -cn <output_CDS_negatives>")			
 
 if __name__ == '__main__':
 	
@@ -89,7 +154,31 @@ if __name__ == '__main__':
 			gbk_file = sys.argv[4]
 			output = sys.argv[2]
 		
-		main(gbk_file, output)
+		sizes = create_kar(gbk_file, output)
+	elif len(sys.argv) == 9:
+		if sys.argv[1] == '-i' and sys.argv[3] == '-o' and sys.argv[5] == '-cp' and sys.argv[7] == '-cn':
+			gbk_file = sys.argv[2]
+			output = sys.argv[4]
+			cds_pos = sys.argv[6]
+			cds_neg = sys.argv[8]
+		elif sys.argv[3] == '-i' and sys.argv[1] == '-o' and sys.argv[5] == '-cp' and sys.argv[7] == '-cn':
+			gbk_file = sys.argv[4]
+			output = sys.argv[2]
+			cds_pos = sys.argv[6]
+			cds_neg = sys.argv[8]
+		elif sys.argv[1] == '-i' and sys.argv[3] == '-o' and sys.argv[7] == '-cp' and sys.argv[5] == '-cn':
+			gbk_file = sys.argv[4]
+			output = sys.argv[2]
+			cds_pos = sys.argv[8]
+			cds_neg = sys.argv[6]
+		elif sys.argv[3] == '-i' and sys.argv[1] == '-o' and sys.argv[7] == '-cp' and sys.argv[5] == '-cn':
+			gbk_file = sys.argv[4]
+			output = sys.argv[2]
+			cds_pos = sys.argv[8]
+			cds_neg = sys.argv[6]
+		
+		sizes = create_kar(gbk_file, output)
+		create_CDS(gbk_file, cds_pos, cds_neg, sizes)
 	else:
 		help()
 	

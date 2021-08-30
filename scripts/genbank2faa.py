1158dc26560615bd15306533966bb418ea4b4ea3
Vicente Saona <vicente.saona@sansano.usm.cl>
2021-08-30 16:55:12 -0400

    gbk2faa bugfix

--- scripts/genbank2faa.py
+++ scripts/genbank2faa.py
@@ -13,13 +13,13 @@ def genbankToFaa(input, output):
     for seq_record in SeqIO.parse(input_handle, "genbank") :
         print("Dealing with GenBank record %s" % seq_record.id)
         for seq_feature in seq_record.features :
-            if seq_feature.type=="CDS" :
-                assert len(seq_feature.qualifiers['translation'])==1
-                fasta = seq_feature.qualifiers['translation'][0]
-                output_handle.write(">%s from %s\n%s\n" % (
-                    seq_feature.qualifiers['locus_tag'][0],
-                    seq_record.name,
-                    "\n".join([fasta[i:i+60] for i in range(0, len(fasta), 60)])))
+            if seq_feature.type=="CDS":
+                if "translation" in seq_feature.qualifiers and len(seq_feature.qualifiers['translation'])==1:
+                    fasta = seq_feature.qualifiers['translation'][0]
+                    output_handle.write(">%s from %s\n%s\n" % (
+                        seq_feature.qualifiers['locus_tag'][0],
+                        seq_record.name,
+                        "\n".join([fasta[i:i+60] for i in range(0, len(fasta), 60)])))
 
     output_handle.close()
     input_handle.close()
@@ -37,4 +37,4 @@ if __name__ == "__main__":
             args.inputFile = args.inputFile[:-3]
     if args.outputFile is None:
         args.outputFile = args.inputFile + "_converted.faa"
-    genbankToFaa(args.inpuFile, args.outputFile)
+    genbankToFaa(args.inputFile, args.outputFile)

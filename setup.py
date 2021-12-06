89bcc212d67f13d300f8e9e70aa847814026d705
Vicente Saona <vicente.saona@sansano.usm.cl>
2021-12-06 17:35:15 -0300

    Experimenting with pip

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 setup(
   name = 'GenoVi',         # How you named your package folder (MyLib)
   packages = find_packages(),   # Chose the same as "name"
-  version = '0.1.5',      # Start with a small number and increase it with every change you make
+  version = '0.1.6',      # Start with a small number and increase it with every change you make
   license='BY-NC-SA Creative Commons License',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Generates full genome images from a gbff file',   # Give a short description about your library
-  author = 'Cumille A. et al.',                   # Type in your name
+  author = 'Cumsille A. et al.',                   # Type in your name
   author_email = 'vicente.saona@sansano.usm.cl',      # Type in your E-Mail
   url = 'https://github.com/robotoD/GenoVi',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/robotoD/GenoVi/archive/refs/tags/0.1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/robotoD/GenoVi/archive/refs/tags/0.1.6.tar.gz',    # I explain this later on
   keywords = ['bioinformatics', 'genomics', 'CIRCOS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'cairosvg',

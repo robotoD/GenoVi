47735379c3f4fb6aa5be673d5cf49a7a9ab6cff1
Andrea Rodríguez <andrea.rodriguez.12@sansano.usm.cl>
2021-12-22 16:58:39 +0000

    Update requirements and minimum versions
    Version 0.1.11

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ extra_ci = [
 setup(
   name = 'genovi',         # How you named your package folder (MyLib)
   packages = find_packages(),   # Chose the same as "name"
-  version = '0.1.10',      # Start with a small number and increase it with every change you make
+  version = '0.1.11',      # Start with a small number and increase it with every change you make
   license='BY-NC-SA Creative Commons License',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Generates full genome images from a gbff file',   # Give a short description about your library
   author = 'Cumsille A. et al.',                   # Type in your name
@@ -26,12 +26,15 @@ setup(
   download_url = 'https://github.com/robotoD/GenoVi/archive/refs/tags/0.1.6.tar.gz',    # I explain this later on
   keywords = ['bioinformatics', 'genomics', 'CIRCOS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'cairosvg',
-          'numpy',
-          'pandas',
-          'BioPython',
+          'cairosvg>=2.5.2',
+          'numpy>=1.20.2',
+          'pandas>=1.2.4',
+          'BioPython>=1.79',
           'argparse',
-          'deepnog==1.2.3'
+          'deepnog>=1.2.3'
+          'scikit-learn',
+          'torch>=1.2.0',
+          'tqdm>=4.35.0'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package

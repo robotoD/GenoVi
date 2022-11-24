b72d78e3432330f6a2f2d55e57c3d652f413782a
Andrea <andrea.rodriguez.12@sansano.usm.cl>
2022-11-24 16:47:38 +0000

    COGs Classification: Added percentage

--- scripts/create_tables.py
+++ scripts/create_tables.py
@@ -3,7 +3,7 @@ import csv
 import matplotlib.pyplot as plt
 
 def fill_unique_chrms(chrms, n):
-	
+
 	def check(lst, n):
 		if len(lst[0]) == n:
 			return list(map(int,lst[1]))
@@ -14,41 +14,41 @@ def fill_unique_chrms(chrms, n):
 			else:
 				continue
 		return list(map(int,new_vals))
-	
+
 	pos = check(chrms[0],n)
 	neg = check(chrms[1],n)
-	
+
 	return [sum(x) for x in zip(pos, neg)]
-		
+
 
 def gral_table(lengths, contents, chrms, output):
 	header = ["Replicon", "Size (bp)", "GC content (%)", "CDS", "tRNA", "rRNA"]
 	ttrnas = fill_unique_chrms(chrms[0], len(lengths))
 	rrnas = fill_unique_chrms(chrms[1], len(lengths))
 	cdss = fill_unique_chrms(chrms[2], len(lengths))
-	
+
 	csv_file = open(output, "w")
 	writer = csv.writer(csv_file)
 	writer.writerow(header)
-	
+
 	for i in range(len(lengths)):
 		avg = contents[i][0]/contents[i][1]
 		line = map(str, [i+1, lengths[i], avg, cdss[i], ttrnas[i], rrnas[i]])
 		writer.writerow(line)
-		
+
 	avg_total = np.sum([x[0] for x in contents])/np.sum([x[1] for x in contents])
 	footer = map(str, ["Total", np.sum(lengths), avg_total, np.sum(cdss), np.sum(ttrnas), np.sum(rrnas)])
-	
+
 	writer.writerow(footer)
 	csv_file.close()
-	
+
 	return
-	
+
 def cogs_classif(hist, output):
-	
+
 	header1 = ["","Cellular Processes and Signaling"]+[""]*9+["Information Storage and Processing"]+[""]*5+["Metabolism"]+[""]*7+["Poorly Characterized","","",""]
 	header2 = ["Replicon","D","M","N","O","T","U","V","W","Y","Z","A","B","J","K","L","X","C","E","F","G","H","I","P","Q","R","S","Unclassified"]
-	
+
 	csv_file = open(output, 'w')
 	writer = csv.writer(csv_file)
 	writer.writerow(header1)
@@ -60,13 +60,16 @@ def cogs_classif(hist, output):
 		i = column[3:]
 		line = map(str,[i] + [hist[hist["COG Category"] == c]["chr"+i].item() for c in header2[1:]])
 		writer.writerow(line)
-		
+
+	total = hist["Frequency"].sum()
 	footer = map(str,["Total"] + [hist[hist["COG Category"] == c]["Frequency"].item() for c in header2[1:]])
-	writer.writerow(footer)
+    footer2 = map(str,["Percentage"] + [round(hist[hist["COG Category"] == c]["Frequency"].item()/total*100,1) for c in header2[1:]])
+    writer.writerow(footer)
+    writer.writerow(footer2)
 	csv_file.close()
-	
+
 	return
-	
+
 def draw_histogram(hist, output):
 	colors = ["#697BB7", '#304E9D', '#6A9AB5', '#1D82B0', '#4CA9B5', '#1C6875', '#6DBFA4', '#1A936F', '#4EB160', '#1C7633', '#E34949', '#CD1B1B', '#AD5B9F', '#A3378C', '#8F76B4',
 	          '#533D91', '#B5D27B', '#83AD29', '#BCB868', '#988F1C', '#EDC28A', '#B7822A', '#DD8950', '#C65F17', '#696969', '#999999', '#EAEAEA']
@@ -76,5 +79,5 @@ def draw_histogram(hist, output):
 	plt.tight_layout()
 	ax.figure.savefig(output+"_COG_Histogram.png", dpi=150)
 	cogs_classif(hist, output+"_COG_Classification.csv")
-	
+
 	return
